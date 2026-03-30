4a03ef89769f1f3fa785b6c1b74c6fd934c26cce
acridge3-369 <acridge3@gmail.com>
2026-03-30 13:51:32 -0700

    Created IsographWebGenerator component file

---
+++ src/components/IsographWebGenerator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IsographWebGenerator: React.FC = () => {
+    return (
+        <div>
+            <h1>Isograph Web Generator</h1>
+            <p>This component will generate Isograph data for web applications.</p>
+        </div>
+    );
+};
+
+export default IsographWebGenerator;
