40380d416e542088e4f8a7b918927cf546e5db60
acridge3-369 <acridge3@gmail.com>
2026-03-30 13:50:36 -0700

    Create Card component file

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,15 @@
+// This is the Card component
+import React from 'react';
+
+const Card = ({ title, children }) => {
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+            <div className="card-content">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
