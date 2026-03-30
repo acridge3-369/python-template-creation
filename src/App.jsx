d8d6f6564e7628edc664ee542e0b9c917add424f
acridge3-369 <acridge3@gmail.com>
2026-03-30 13:29:50 -0700

    Create src/App.jsx file with Isograph Template Generator component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IsographTemplateGenerator = () => {
+    return (
+        <div>
+            <h1>Isograph Template Generator</h1>
+        </div>
+    );
+};
+
+export default IsographTemplateGenerator;
