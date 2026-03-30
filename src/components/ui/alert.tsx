b6c65bd7f3997313c36fe9b24e79418d97d4844b
acridge3-369 <acridge3@gmail.com>
2026-03-30 13:51:28 -0700

    Create alert component file

---
+++ src/components/ui/alert.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Alert = ({ message, type }) => {
+    return (
+        <div className={`alert alert-${type}`}>
+            {message}
+        </div>
+    );
+};
+
+Alert.propTypes = {
+    message: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['success', 'error', 'info', 'warning']).isRequired,
+};
+
+export default Alert;
